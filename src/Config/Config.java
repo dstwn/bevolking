1ded75650bd4f1e474ade16997da4455b74b514b
Dwi Setiawan <dwisetiawan@linuxmail.org>
2020-05-29 00:40:57 +0700

    add config

--- src/Config/Config.java
+++ src/Config/Config.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package Config;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author macbookpro
  */
 public class Config {
-    
+    private static Connection mysqlconfig;
+    public static Connection configDB()throws SQLException{
+        try {
+            String url="jdbc:mysql://localhost:3306/basis_data_penduduk"; //url database
+            String user="root"; //user database
+            String pass="-3Llysa8"; //password database
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlconfig=DriverManager.getConnection(url, user, pass);            
+        } catch (Exception e) {
+            System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+        }
+        return mysqlconfig;
+    }    
 }
